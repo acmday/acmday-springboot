388761414609d607047dad4fb9cb9bdb5bc6c979
acmday <acmday@sina.com>
2020-04-11 18:45:30 +0800

    create project

---
+++ dao/src/main/java/com/acmday/springboot/dao/Hello.java
@@ -0,0 +1,8 @@
+package com.acmday.springboot.dao;
+
+/**
+ * @author acmday.
+ * @date 2020/4/11.
+ */
+public class Hello {
+}
---
+++ server/src/main/java/com/acmday/springboot/server/Main.java
@@ -0,0 +1,12 @@
+package com.acmday.springboot.server;
+
+/**
+ * @author acmday.
+ * @date 2020/4/11.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
