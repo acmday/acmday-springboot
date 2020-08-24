dbe13c730d2064e768364592e7c8d7fd073e3373
acmday <acmday@sina.com>
2020-08-24 11:41:59 +0800

    refactor

--- server/src/main/java/com/acmday/springboot/server/Main.java
+++ server/src/main/java/com/acmday/springboot/server/Main.java
@@ -1,6 +1,7 @@
 package com.acmday.springboot.server;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @Slf4j
 @ComponentScan("com.acmday.springboot")
-//@MapperScan(basePackages = "com.acmday.springboot.dao.mapper")
+@MapperScan(basePackages = {"com.acmday.springboot.dao.mapper"})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
